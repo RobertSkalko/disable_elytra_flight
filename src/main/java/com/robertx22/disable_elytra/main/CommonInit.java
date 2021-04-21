53ede4767ca5ee63d521ee1c088596c2e60d94c8
robert trebor <treborx555@gmail.com>
2021-04-21 14:27:07 +0200

    Initial commit

---
+++ src/main/java/com/robertx22/disable_elytra/main/CommonInit.java
@@ -0,0 +1,11 @@
+package com.robertx22.disable_elytra.main;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CommonInit implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        System.out.print("Elytra's flight disabled.");
+    }
+}
